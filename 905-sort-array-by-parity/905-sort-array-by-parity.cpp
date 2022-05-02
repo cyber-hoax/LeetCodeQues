3b88212ab366882f3ca0418132c0753b9a039f7e
Raunak Tamang <raunaktamang@gmail.com>
2022-05-02 13:21:28 +0530

    Time: 14 ms (51.99%), Space: 16.9 MB (7.31%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -2,29 +2,24 @@ class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         
-        stack<int> even;
-        stack<int> odd ;
+        queue<int> q;
         vector<int> v ;
         
         for(int i = 0; i<nums.size(); i++){
             if(nums[i] % 2 == 0)
-                even.push(nums[i]);
+                v.push_back(nums[i]);
             else{
-                odd.push(nums[i]);
+                q.push(nums[i]);
             }
         }
         
-        while(!even.empty()){
-            v.push_back(even.top());
-            even.pop();
+        while(!q.empty()){
+            v.push_back(q.front());
+            q.pop();
         }
         
           
-        while(!odd.empty()){
-            v.push_back(odd.top());
-            odd.pop();
-        }
-        
+      
         return v ;
     }
 };
