92ddc43bb0c892d982ee874c555e47ddc626fd1e
Raunak Tamang <raunaktamang@gmail.com>
2022-05-03 16:09:46 +0530

    Time: 57 ms (22.67%), Space: 28.8 MB (6.92%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -8,29 +8,23 @@ public:
         v.push_back(nums[i]);
     }
     
-    sort(v.begin() , v.end());
+    sort(v.begin() , v.end());  //sort vector
     
-    int start = -1 ;
+    int start = -1 ;           //start with -1 since 0 incex can also be unsorted
     int end = 0 ;
     
-    
     for( int i= 0 ;i< n ; i++){
-        
         if(nums[i] != v[i]){
-            
-            if(start < 0){
+            if(start < 0)
                 start = i ;
-            }
-            else{
-                end=i;
-                
-            }
+            else
+                end=i;     
         }
-    
     }
+        
     if(end == 0){
         return  0 ;
     }
-    return  end - start+1 ;
+    return end - start+1 ;
     }
 };
