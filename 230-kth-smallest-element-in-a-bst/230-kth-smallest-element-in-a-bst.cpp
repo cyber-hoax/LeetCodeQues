8c1f1e26b36cb15e6c311787326ecad275aed5b8
Raunak Tamang <raunaktamang@gmail.com>
2022-06-04 13:49:31 +0530

    Time: 26 ms (28.71%), Space: 24.1 MB (62.27%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -19,16 +19,31 @@ class Solution {
         helper(root->right, q);
         
     }
+    
+    TreeNode* kthsmall(TreeNode* root, int k , int &pos){
+        if(!root)
+            return root;
+        
+        TreeNode* left = kthsmall(root->left, k , pos);
+        if(left !=NULL)
+             return left ;
+        pos ++;
+        if(pos == k)
+            return root;
+        return kthsmall(root->right, k , pos);
+    }
 public:
     int kthSmallest(TreeNode* root, int k) {
-       priority_queue<int, vector<int>, greater<int>>q;
-        helper(root, q);
-        int ans = 0 ;
-        while(k>=1){
-            ans = q.top();
-            q.pop();
-            k--;
-        }
-        return ans;
+       // priority_queue<int, vector<int>, greater<int>>q;
+       //  helper(root, q);
+       //  int ans = 0 ;
+       //  while(k>=1){
+       //      ans = q.top();
+       //      q.pop();
+       //      k--;
+       //  }
+       //  return ans;
+        int pos = 0;
+        return kthsmall(root, k , pos)->val;
     }
 };
