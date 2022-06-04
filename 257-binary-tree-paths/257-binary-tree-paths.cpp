e31c7ce6bd472fb5357abbe7747dc7b0a5445bfc
Raunak Tamang <raunaktamang@gmail.com>
2022-06-04 21:09:57 +0530

    Time: 4 ms (71.78%), Space: 14 MB (8.45%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.cpp
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -10,75 +10,27 @@
  * };
  */
 class Solution {
+    string helper(TreeNode* root,  vector<string> &ans , string s){
+        if(!root)
+            return  "";
+      
+        s += to_string(root->val) + "->";
+        
+        if(root->left == NULL && root->right == NULL){
+            s.pop_back();
+            s.pop_back();
+            ans.push_back(s);
+        }
+        
+        return helper(root->left, ans,s), helper(root->right, ans,s);
     
-    vector<string> binaryTree(TreeNode* root){
-        if(!root){
-             return {};
-            
-        }
-        
-        if(root->left == NULL && root->right ==NULL ){
-            return {to_string(root->val)};
-        }
-        
+    }
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
         vector<string> ans;
-        
-        vector<string>  left = binaryTree(root->left);
-        vector<string> right = binaryTree(root->right);
-        
-        for(auto x : left ){
-            ans.push_back(to_string(root->val)+"->"+x);
-        }
-        
-            for(auto x : right ){
-            ans.push_back(to_string(root->val)+"->"+x);
-        }
-        
+        string s ="";
+        helper(root, ans,s);
         return ans;
         
     }
-    
-    vector<string> paths ;
-    void topDown (TreeNode* root, string s){
-        if(!root){
-            return ;
-        }
-        
-        
-          string temp = s+ (s.length() == 0  ? "" :  "->")+to_string(root->val);
-        if(root->left == NULL && root->right ==NULL ){
-            paths.push_back(temp);
-            return ;
-        }
-        
-         topDown(root->left, temp);
-        topDown(root->right, temp);
-        
-        
-        
-        
-    }
-    
-    
-    
-public:
-    vector<string> binaryTreePaths(TreeNode* root) {
-        
-        
-//         vector<string> v = binaryTree(root);
-        topDown(root, "");
-        
-        return paths;
-        
-    }
 };
-
-
-//
-/*
-            1
-           /  \ 
-          2     3
-            \
-              5
-*/
