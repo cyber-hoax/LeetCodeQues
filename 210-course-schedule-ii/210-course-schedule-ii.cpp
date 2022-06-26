0891a9d674be5b0506c44fd4f73640f146f08c77
Raunak Tamang <raunaktamang@gmail.com>
2022-06-26 23:36:38 +0530

    Time: 35 ms (43.11%), Space: 13.9 MB (67.63%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,56 +1,57 @@
 class Solution {
 public:
-   vector<int> findOrder(int num, vector<vector<int>>& pre) {
-       int n = num;
-        vector<int> adj[num];
-        vector<int> deg(num), order;
-        for(auto x : pre){
-            adj[x[1]].push_back(x[0]);
-            deg[x[0]]++;
-        }
-        queue<int>q;
-        for(int i = 0; i<num; i++) {
-            if(deg[i] == 0) q.push(i);
-        }
-        while(!q.empty()){
-            int f = q.front();
-            q.pop();
-            order.push_back(f);
-            n--;
-            for(auto v : adj[f]){
-                if(--deg[v] == 0) q.push(v);
-            }
-        }
-        if(n != 0) return {};
-        return order; 
-   }
+   vector<int> findOrder(int A, vector<vector<int>>& B) {
+   //     int n = num;
+   //      vector<int> adj[num];
+   //      vector<int> deg(num), order;
+   //      for(auto x : pre){
+   //          adj[x[1]].push_back(x[0]);
+   //          deg[x[0]]++;
+   //      }
+   //      queue<int>q;
+   //      for(int i = 0; i<num; i++) {
+   //          if(deg[i] == 0) q.push(i);
+   //      }
+   //      while(!q.empty()){
+   //          int f = q.front();
+   //          q.pop();
+   //          order.push_back(f);
+   //          n--;
+   //          for(auto v : adj[f]){
+   //              if(--deg[v] == 0) q.push(v);
+   //          }
+   //      }
+   //      if(n != 0) return {};
+   //      return order; 
+   // }
        
-//       vector<int> g[A];
-//     vector<int> indegree(A,0);
-//     vector<int> ans;
-//     for(auto x: B){
-//         g[x[1]].push_back(x[0]);
-//         indegree[x[0]] ++;
-//     }
+    vector<int> g[A];
+    vector<int> indegree(A,0);
+    vector<int> ans;
+    for(auto x: B){
+        g[x[1]].push_back(x[0]);
+        indegree[x[0]] ++;
+    }
 
-//     queue<int> q;
-//     for(int i =1; i<A; i++){
-//         if(indegree[i] == 0 )
-//             q.push(i);
+    queue<int> q;
+    for(int i =0; i<A; i++){
+        if(indegree[i] == 0 )
+            q.push(i);
            
-//     }
-//     while(!q.empty()){
-//         int ele = q.front();
-//         q.pop();
-//         ans.push_back(ele);
-//         A-- ;
-//         for(auto child: g[ele]){
-//             if(--indegree[child] == 0)
-//             q.push(child);
+    }
+    while(!q.empty()){
+        int ele = q.front();
+        q.pop();
+        ans.push_back(ele);
+        A-- ;
+        for(auto child: g[ele]){
+            if(--indegree[child] == 0)
+            q.push(child);
            
-//         }
-//     }
-//     if(A != 0) return {};
-//         return ans;   
+        }
+    }
+    if(A != 0) return {};
+        return ans;   
+   }   
 
 };
