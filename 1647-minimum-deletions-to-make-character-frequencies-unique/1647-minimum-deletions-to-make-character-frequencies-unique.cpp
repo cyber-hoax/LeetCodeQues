794ee878651acc445826dfe755017f4f56cc3fba
Raunak Tamang <raunaktamang@gmail.com>
2022-06-28 15:51:56 +0530

    Time: 85 ms (57.37%), Space: 17.1 MB (95.28%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -7,25 +7,36 @@ public:
         for(auto x: s)
             freq[x - 'a'] ++ ;
         
-        priority_queue<int> q;
+//         priority_queue<int> q;
         
-       for(int i = 0; i< 26; i++){
-           if(freq[i]> 0)
-               q.push(freq[i]);
-       }
+//        for(int i = 0; i< 26; i++){
+//            if(freq[i]> 0)
+//                q.push(freq[i]);
+//        }
         
-        while(q.size() >1){
-            int ele = q.top();
-            q.pop();
-            if(ele == q.top()){
-                if(ele - 1 >0)
-                    q.push(ele -1);
-                count ++ ;
-                }
+//         while(q.size() >1){
+//             int ele = q.top();
+//             q.pop();
+//             if(ele == q.top()){
+//                 if(ele - 1 >0)
+//                     q.push(ele -1);
+//                 count ++ ;
+//                 }
             
             
+//         }
+        
+        sort(freq.begin(), freq.end());
+        
+        for(int i = 24 ; i >=0 ;i--){
+            if(freq[i] == 0)
+                break;
+            if(freq[i] >= freq[i+1]){
+                int prev = freq[i];
+                freq[i] = max(0, freq[i+1]-1);
+                count += prev - freq[i];
+            }
         }
-        
         
         return count;
     }
