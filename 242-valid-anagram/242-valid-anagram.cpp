f24b5f837e3ce5d942d7f0d1fdf0348f2e39f59f
Raunak Tamang <raunaktamang@gmail.com>
2022-07-28 12:35:42 +0530

    Time: 10 ms (74.53%), Space: 7.4 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -4,22 +4,30 @@ public:
         if(s.size() != t.size())
             return false;
         
-        unordered_map<int,int> hm;
-        unordered_map<int, int> hm2;
-        for(auto x : s){
-            hm[x]++;
+//         unordered_map<int,int> hm;
+//         unordered_map<int, int> hm2;
+//         for(auto x : s){
+//             hm[x]++;
+//         }
+        
+//         for(auto x : t)
+//             hm2[x] ++ ;
+        
+        
+//         for(auto i : s){
+//             if(hm[i] != hm2[i]){
+//                 return false;
+//             }
+//         }
+        
+         int n = s.length();
+        int counts[26] = {0};
+        for (int i = 0; i < n; i++) { 
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--;
         }
-        
-        for(auto x : t)
-            hm2[x] ++ ;
-        
-        
-        for(auto i : s){
-            if(hm[i] != hm2[i]){
-                return false;
-            }
-        }
-        
+        for (int i = 0; i < 26; i++)
+            if (counts[i]) return false;
         return true;
     }
 };
