484bb6632f25b769a118ab546b416b0ebe90a53d
Raunak Tamang <raunaktamang@gmail.com>
2022-08-04 13:07:41 +0530

    Time: 40 ms (11.08%), Space: 7.8 MB (49.47%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        if (s.empty()) return 0;
+        
+        unordered_map<char,int> table {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        
+        int ans = 0 ; 
+        
+        for(int i  = 0 ; i< s.size()-1 ; ++i){
+            if(table[s[i]] < table[s[i+1]])
+                ans -= table[s[i]];
+            else
+                ans += table[s[i]];
+        }
+        
+        ans += table[s.back()];
+        
+        
+        return ans;
+        
+    }
+};
