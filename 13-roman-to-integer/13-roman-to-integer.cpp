7677e6af641de9f5c56a987c045b44cbf84f1634
Raunak Tamang <raunaktamang@gmail.com>
2022-08-04 13:14:30 +0530

    Time: 7 ms (90.35%), Space: 6.6 MB (54.70%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,31 +1,57 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        if (s.empty()) return 0;
+//         if (s.empty()) return 0;
         
-        unordered_map<char,int> table {
-            {'I', 1},
-            {'V', 5},
-            {'X', 10},
-            {'L', 50},
-            {'C', 100},
-            {'D', 500},
-            {'M', 1000}
-        };
+//         unordered_map<char,int> table {
+//             {'I', 1},
+//             {'V', 5},
+//             {'X', 10},
+//             {'L', 50},
+//             {'C', 100},
+//             {'D', 500},
+//             {'M', 1000}
+//         };
         
-        int ans = 0 ; 
+//         int ans = 0 ; 
         
-        for(int i  = 0 ; i< s.size()-1 ; ++i){
-            if(table[s[i]] < table[s[i+1]])
-                ans -= table[s[i]];
-            else
-                ans += table[s[i]];
+//         for(int i  = 0 ; i< s.size()-1 ; ++i){
+//             if(table[s[i]] < table[s[i+1]])
+//                 ans -= table[s[i]];
+//             else
+//                 ans += table[s[i]];
+//         }
+        
+//         ans += table[s.back()];
+        
+        
+//         return ans;
+        
+        
+    vector<int> roman(22,0);
+    roman['I' - 'C'] = 1;
+    roman['V' - 'C'] = 5;
+    roman['X' - 'C'] = 10;
+    roman['L' - 'C'] = 50;
+    roman['C' - 'C'] = 100;
+    roman['D' - 'C'] = 500;
+    roman['M' - 'C'] = 1000;
+        
+        int value= 0 ;
+        for(int i = 0 ; i <s.size()-1 ; ++i){
+             if(roman[s[i] - 'C']>=roman[s[i+1] - 'C'])
+        {
+            value = value + roman[s[i] - 'C'];
+        }
+        else
+        {
+            value = value - roman[s[i] - 'C'];
+        }
         }
         
-        ans += table[s.back()];
+        value += roman[s[s.size()-1] - 'C'];
         
-        
-        return ans;
-        
+        return value;
+    
     }
 };
