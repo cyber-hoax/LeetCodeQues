9156b52480aaf987d7adad263ea48b1e8e40edbc
Raunak Tamang <raunaktamang@gmail.com>
2022-08-24 15:48:56 +0530

    Time: 19 ms (69.30%), Space: 9.4 MB (53.62%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,14 +1,21 @@
 class Solution {
+    int helper(vector<int> nums, int s){
+        vector<int> dp(s+1, 0);
+        dp[0] = 1;
+        
+        for(int i = 0 ; i< nums.size(); ++i){
+            for(int j = s; j>= nums[i]; --j){
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+        return dp[s];
+    }
 public:
       int findTargetSumWays(vector<int>& nums, int S) {
-        int n = nums.size();
-        vector<unordered_map<int,int>> dp(n+1);
-        dp[0][0]=1;
-        for(int i=0;i<n;i++)
-            for(auto &p:dp[i]) {
-                dp[i+1][p.first+nums[i]] += p.second; 
-                dp[i+1][p.first-nums[i]] += p.second;
-            }
-        return dp[n][S];
-    }
+          int sum = accumulate(nums.begin(), nums.end(),0);
+          if(sum < abs(S) || (sum + S) % 2 ==1)
+              return 0;
+          else
+              return helper(nums, (sum+S) /2 );
+      }
 };
