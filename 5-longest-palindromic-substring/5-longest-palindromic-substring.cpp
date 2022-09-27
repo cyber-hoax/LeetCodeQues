8e4fa4a69e11388a52b913012c731b0b72c98236
Raunak Tamang <raunaktamang@gmail.com>
2022-09-27 13:16:36 +0530

    Time: 1187 ms (7.35%), Space: 6.8 MB (91.58%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,44 +1,31 @@
 class Solution {
+    bool check(string& s, int i, int j){
+        while(i<j){
+            if(s[i] != s[j])
+                return false;
+            j--;
+            i++;
+        }
+        return true;
+    }
 public:
     string longestPalindrome(string s) {
+       int i = 0 , j = 0 , start =0 , end = 0 ;
+    string ans ="";
+       for(i = 0 ; i < s.size(); ++i){
+          for(j =i ; j<s.size(); ++j){
+              if(check(s,i ,j)){
+                  if(j-i > end-start){
+                      start = i;
+                      end = j;
+                  }       
+              }
+          }
+       } 
         
-        int n = s.size();
-        if (n == 0)
-            return "";
-
-        // dp[i][j] will be 'true' if the string from index i to j is a palindrome.
-        bool dp[n][n];
-
-        //Initialize with false
-
-        memset(dp, 0, sizeof(dp));
-
-        //Every Single character is palindrome
-        for (int i = 0; i < n; i++)
-            dp[i][i] = true;
-
-        string ans = "";
-        ans += s[0];
-
-        for (int i = n - 1; i >= 0; i--)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                if (s[i] == s[j])
-                {
-                    //If it is of two character OR if its susbtring is palindrome.
-                    if (j - i == 1 || dp[i + 1][j - 1])
-                    {
-                        // //Then it will also a palindrome substring
-                        dp[i][j] = true;
-
-                        //Check for Longest Palindrome substring
-                        if (ans.size() < j - i + 1)
-                            ans = s.substr(i, j - i + 1);
-                    }
-                }
-            }
-        }
+        for(int k = start ; k<=end ; ++k)
+            ans += s[k];
+        
         return ans;
     }
 };
