bf99540441543ab4307f81c258394c84c9192589
Raunak Tamang <raunaktamang@gmail.com>
2022-10-09 13:52:30 +0530

    Time: 45 ms (69.98%), Space: 24.8 MB (93.64%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        
-        
-        int n = nums.size();
-         int times = k % n;
-        int leftSideLeft = 0, leftSideRight = n - times - 1;
-        int rightSideLeft = n-times , rightSideRight = n -1;
-        
-        
-      cout << leftSideRight ;
-        
-        while(leftSideLeft<leftSideRight){
-            swap(nums[leftSideLeft++], nums[leftSideRight--]);
+        int i = 0 , j = nums.size()-1;
+        while(i<j){
+            swap(nums[i], nums[j]);
+            i++;
+            j--;
         }
         
-        while(rightSideLeft < rightSideRight)
-            swap(nums[rightSideRight--], nums[rightSideLeft++]);
+        k %= nums.size();
+        i= 0;
+        j = k-1;
+        while(i<j){
+             swap(nums[i], nums[j]);
+            i++;
+            j--;
+        }
         
-      
-         int i =0 ,j =nums.size()-1;
-        while(i<j)
-            swap(nums[i++],(nums[j--]));
-    
+        i = k;
+        j = nums.size()-1;
+        
+         while(i<j){
+             swap(nums[i], nums[j]);
+            i++;
+            j--;
+        }
     }
 };
-
-
-// num size =4  13
-// 13 % 4
