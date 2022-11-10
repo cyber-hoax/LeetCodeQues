51afefed54a110edac9ee19d57d8612c9eaf8c35
Raunak Tamang <raunaktamang@gmail.com>
2022-11-10 10:45:08 +0530

    Time: 47 ms (61.58%), Space: 11 MB (48.01%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        stack<char> st;
-        string ans;
-        for(auto x : s){
-            if(st.empty())
-                st.push(x);
-            else{
-                if(!st.empty() && st.top() == x){
-                    st.pop();
-                    
-                }
-                else
-                    st.push(x);
-                    
-            }
-            
-        }
-        while(!st.empty()){
-            ans += st.top();
-            st.pop();
-        }
-        reverse(ans.begin(), ans.end());
-            return ans;
+         string res = "";
+        for (char& c : s)
+            if (res.size() && c == res.back())
+                res.pop_back();
+            else
+                res.push_back(c);
+        return res;
     }
 };
